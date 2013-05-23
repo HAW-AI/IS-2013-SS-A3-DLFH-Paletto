250ed8a2b85b3731c659055f1ce3f91168dfcfc1
Neonchen <no@reply.com>
2013-05-23 20:40:49 +0200

    colors for up to 13 tokens implemented

--- src/com/haw/paletto/logic/GameLogic.java
+++ src/com/haw/paletto/logic/GameLogic.java
@@ -1,17 +1,38 @@
 package com.haw.paletto.logic;
 
+import static java.awt.Color.*;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import com.haw.paletto.Token;
 
 public class GameLogic {
+	
+	List<Color> colorList = Arrays.asList(red,blue,green,yellow,black,white,gray,orange,pink,magenta,lightGray,cyan,darkGray); 
 
 	public GameLogic(){
 		//TODO
 	}
 	
 	public List<List<Token>> buildBoard(int size){
-		//TODO
+		List<List<Token>> tokens = new ArrayList<List<Token>>();
+		if(size > colorList.size()){
+			List<Color> colors = getColors(size);
+		}
+		return tokens;
+	}
+	
+	private List<Color> getColors(int quantity){
+		List<Color> result = new ArrayList<Color>();
+		for(int i=0; i< quantity;i++){
+			for(int j=0; j < quantity;j++){
+				result.add(colorList.get(i));
+			}
+		}
+		return result;
 	}
 	
 	public boolean gameWon(){
