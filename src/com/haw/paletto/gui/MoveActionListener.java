ee3ddd9320d53305918284b014b5b3e12c3f1f92
Neonchen <no@reply.com>
2013-05-23 16:06:24 +0200

    implemented human player button listener to take a stone

--- src/com/haw/paletto/gui/MoveActionListener.java
+++ src/com/haw/paletto/gui/MoveActionListener.java
@@ -3,18 +3,21 @@ package com.haw.paletto.gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import com.haw.paletto.Game;
+
 public class MoveActionListener implements ActionListener{
 	
-	private GameGui gui;
+	private Game game;
 
-	public MoveActionListener(GameGui gui){
-		this.gui = gui;
+	public MoveActionListener(Game game){
+		this.game = game;
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		
+	public void actionPerformed(ActionEvent event) {
+		String FieldPosition = event.getActionCommand();
+		int xPos = Integer.parseInt(FieldPosition.split("-")[0]);
+		int yPos = Integer.parseInt(FieldPosition.split("-")[1]);
+		game.takeStone(xPos, yPos);
 	}
-
 }
