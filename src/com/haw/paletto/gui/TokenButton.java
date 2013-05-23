0d7c2d205fd283e856c8333aa6512bd79a78f8ea
Neonchen <no@reply.com>
2013-05-23 13:01:00 +0200

    added token button behaviour

--- src/com/haw/paletto/gui/TokenButton.java
+++ src/com/haw/paletto/gui/TokenButton.java
@@ -8,6 +8,7 @@ public class TokenButton extends JButton {
 	 * 
 	 */
 	private static final long serialVersionUID = -3468209067687167196L;
+	private boolean available = false;
 
 	public TokenButton(){
 		super("");
@@ -15,5 +16,34 @@ public class TokenButton extends JButton {
 	
 	public TokenButton(String label){
 		super(label);
+	}
+	
+	public boolean setMoveable(){
+		boolean result = false;
+		if(this.isAvailable()){
+			this.setEnabled(true);
+			result = true;
+		}
+		return result;
+	}
+	
+	public boolean setUnmoveable(){
+		boolean result = false;
+		if(this.isAvailable()){
+			this.setEnabled(false);
+		}
+		return result;
+	}
+	
+	public boolean remove(){
+		boolean result = false;
+		if(this.isAvailable()){
+			this.available = true;			
+		}
+		return result;
+	}
+	
+	public boolean isAvailable(){
+		return this.available==true;
 	}
 }
