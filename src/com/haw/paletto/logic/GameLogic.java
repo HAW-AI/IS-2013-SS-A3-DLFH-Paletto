233505b3a651b5de20da784108d3256ae37d9684
Neonchen <no@reply.com>
2013-05-24 12:36:03 +0200

    building a board is now possible but still performs bad with bigger boards

--- src/com/haw/paletto/logic/GameLogic.java
+++ src/com/haw/paletto/logic/GameLogic.java
@@ -5,6 +5,7 @@ import static java.awt.Color.*;
 import java.awt.Color;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import com.haw.paletto.Token;
@@ -18,11 +19,20 @@ public class GameLogic {
 	}
 	
 	public List<List<Token>> buildBoard(int size){
-		List<List<Token>> tokens = new ArrayList<List<Token>>();
-		if(size > colorList.size()){
+		List<List<Token>> result = new ArrayList<List<Token>>();
+		if(size <= colorList.size()){
 			List<Color> colors = getColors(size);
+			colors = palettoShuffle(colors, size);
+			for(int i=0; i< size;i++){
+				List<Token> newList = new ArrayList<Token>(); 
+				for(int j=0; j < size;j++){
+					newList.add(new Token(colors.get((size*i)+j),j,i));
+				}
+				result.add(newList);
+			}
 		}
-		return tokens;
+		
+		return result;
 	}
 	
 	private List<Color> getColors(int quantity){
@@ -35,6 +45,33 @@ public class GameLogic {
 		return result;
 	}
 	
+	private List<Color> palettoShuffle(List<Color> colors, int size){
+		boolean validOrder = false;
+		
+		while(!validOrder){
+			validOrder = true;
+			Collections.shuffle(colors);
+			System.out.println(colors.size());
+			for (int i = 0; (i < colors.size()); i++) {
+				Color currentElem = colors.get(i);
+				System.out.println(">>");
+				
+				if(i==0){ //first value
+					if(colors.get(i+1).equals(currentElem) || colors.get(i+size).equals(currentElem) ) validOrder = false;
+				} else if(i < size){ //rest of first row
+					if(colors.get(i-1).equals(currentElem) || colors.get(i+1).equals(currentElem) || colors.get(i+size).equals(currentElem) ) validOrder = false;
+				} else if(i >= size && i < (size*size-size-1)){ //all rows between first and last
+					if(colors.get(i-1).equals(currentElem) || colors.get(i+1).equals(currentElem) || colors.get(i-size).equals(currentElem) || colors.get(i+size).equals(currentElem) ) validOrder = false;
+				} else if(i==(size*size-1)){ //last value
+					if(colors.get(i-1).equals(currentElem) || colors.get(i-size).equals(currentElem) ) validOrder = false;
+				} else { //rest of last row
+					if(colors.get(i-1).equals(currentElem) || colors.get(i+1).equals(currentElem) || colors.get(i-size).equals(currentElem) ) validOrder = false;
+				}
+			}
+		}
+		return colors;
+	}
+		
 	public boolean gameWon(){
 		//TODO
 	}
