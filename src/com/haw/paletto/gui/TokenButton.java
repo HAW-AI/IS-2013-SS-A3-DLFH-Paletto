b9c1088489d560f80e83256d079f2cdaf1f670cd
Neonchen <no@reply.com>
2013-05-27 17:25:13 +0200

    fancy round buttons

--- src/com/haw/paletto/gui/TokenButton.java
+++ src/com/haw/paletto/gui/TokenButton.java
@@ -1,10 +1,15 @@
 package com.haw.paletto.gui;
 
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
 
 import javax.swing.*;
 
-
+//With the help of:
+//http://stackoverflow.com/questions/778222/make-a-button-round
 public class TokenButton extends JButton {
 
 	/**
@@ -13,18 +18,62 @@ public class TokenButton extends JButton {
 	private static final long serialVersionUID = -3468209067687167196L;
 	
 	public TokenButton(){
-		super("");
+		this("");
 	}
 	
 	public TokenButton(String label){
 		super("");
-		this.setFocusPainted(false);
-		this.setBorderPainted(false);
+		Dimension size = getPreferredSize();
+	    size.width = size.height = Math.max(size.width, 
+	      size.height);
+	    setPreferredSize(size);
 
+	// This call causes the JButton not to paint 
+	   // the background.
+	// This allows us to paint a round background.
+	    setContentAreaFilled(false);
 	}
 	
 	public void setState(Color color, boolean enabled){
 		this.setBackground(color);
 		this.setEnabled(enabled);
 	}
+	
+	// Paint the round background and label.
+	  protected void paintComponent(Graphics g) {
+	    if (getModel().isArmed()) {
+	// You might want to make the highlight color 
+	   // a property of the RoundButton class.
+	      g.setColor(Color.lightGray);
+	    } else {
+	      g.setColor(getBackground());
+	    }
+	    g.fillOval(0, 0, getSize().width-1, 
+	      getSize().height-1);
+
+	// This call will paint the label and the 
+	   // focus rectangle.
+	    super.paintComponent(g);
+	  }
+
+	// Paint the border of the button using a simple stroke.
+	  protected void paintBorder(Graphics g) {
+	    g.setColor(getForeground());
+	    g.drawOval(0, 0, getSize().width-1, 
+	      getSize().height-1);
+	  }
+
+	// Hit detection.
+	  Shape shape;
+	  public boolean contains(int x, int y) {
+	// If the button has changed size, 
+	   // make a new shape object.
+	    if (shape == null || 
+	      !shape.getBounds().equals(getBounds())) {
+	      shape = new Ellipse2D.Float(0, 0, 
+	        getWidth(), getHeight());
+	    }
+	    return shape.contains(x, y);
+	  }
+
 }
