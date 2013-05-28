dffdc91d35c8503cebdf713e6ae123c4acf0f779
Neonchen <no@reply.com>
2013-05-28 19:50:32 +0200

    added console output and refactored code for better debugging of evaluation

--- src/com/haw/paletto/logic/AI.java
+++ src/com/haw/paletto/logic/AI.java
@@ -1,11 +1,12 @@
 package com.haw.paletto.logic;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.haw.paletto.Game;
 import com.haw.paletto.Token;
@@ -14,84 +15,93 @@ import java.awt.*;
 
 public class AI {
 	
-	static int searchDepth = 4;
+	static int searchDepth = 3;
 	
 	public static Game doBestMove(Game game){
 		return doMove(game, bestMove(game, searchDepth, true),true);
 	}
 	
-	public static Game doMove(Game game, List<Token> move, boolean aiOnTurn) {
-		return Game.takeStones(game, move, aiOnTurn);
+	public static Game doMove(Game game, List<Token> move, boolean isAi) {
+		return Game.takeStones(game, move, isAi);
 	}
-	
-	public static List<Token> bestMove(Game game, int depth, boolean aiOnTurn){
+		
+	public static List<Token> bestMove(Game game, int depth, boolean isAi){
 		int val = Integer.MIN_VALUE;
 		List<Token> result = null;
-		for(List<Token> posMove : possibleMoves(game.getBoard())){
-			Game newGame = doMove(Game.clone(game), posMove, aiOnTurn);
-			int eval = evalNextState(newGame,aiOnTurn,depth-1, posMove, game);
-			if(eval>val){ val=eval; result = posMove;}
+		for(List<Token> aMove : possibleMoves(game.getBoard())){
+			Game newGame = doMove(Game.clone(game), aMove, isAi);
+			int eval = evalNextState(newGame,isAi,depth-1, aMove.get(0).color(), aMove.size(), game);
+			System.out.println("BestMove "+aMove+" = "+eval);
+			if(eval > val){
+				val=eval;
+				result = aMove;
+			}
 		}
+		System.out.println("---------------FinalBestMove "+result+" = "+val);
 		return result;
 	}
 	
-	public static int evalNextState(Game game, boolean ai, int searchDepth, List<Token> move, Game oldGame){
-		return minmax(game, ai, searchDepth, move, oldGame);
+	public static int evalNextState(Game game, boolean ai, int searchDepth, Color color, int size, Game oldGame){
+		return minmax(game, ai, searchDepth, color, size, oldGame);
 	}
 	
-	public static int minmax(Game game, boolean ai, int searchDepth, List<Token> move, Game oldGame){
-		if(searchDepth <= 0 || Game.playerWon(game)){
-			return evalMove(oldGame,ai,move);
+	public static int minmax(Game game, boolean ai, int searchDepth, Color color, int size, Game oldGame){
+		//System.out.println("Depth:"+searchDepth);
+		if(searchDepth <= 0 || Game.playerWon(game)){ //end recursion if depth = 0 or one player has won
+			return evalMove(oldGame, ai, color, size, searchDepth);
 		}
-		int resultVal;
-		if(ai) resultVal = Integer.MAX_VALUE;
-		else resultVal = Integer.MIN_VALUE;
-		
-		for(List<Token> posMove : possibleMoves(game.getBoard())){
-			if(Game.moveAllowed(posMove)){
-				Game newGame = doMove(game, posMove, ai);
-				int nextVal=minmax(newGame, !ai, searchDepth-1, posMove, game);
-				if( (!ai && nextVal>=resultVal) || (ai && nextVal<=resultVal)){
-					resultVal = nextVal;
-				}
-			}
-		}
-		return resultVal;
-		
-	}
-	
-	public static int evalMove(Game game, boolean ai, List<Token> move){
-		int result = 0;
-		Color gainedColor = move.get(0).color();
-		Map<Color,Integer> ownList, opponentList;
-		if(ai){
-			ownList = game.getAiStones();
-			opponentList = game.getPlayerStones();
-		}else{
-			ownList = game.getPlayerStones();
-			opponentList = game.getAiStones();
-		}
-		if(ownList.containsKey(gainedColor)){
-			//gut, wenn eigene farbe erhöht
-			//schlecht, wenn eigene farbe und gegner schon hat
-			if(opponentList.containsKey(gainedColor)){
-				result = 0;
-			}else{
-				result = move.size() + ownList.get(gainedColor);
-				if(result == game.getSize()) result = Integer.MAX_VALUE;
-			}
+		boolean isMax = ai? true : false; //AI is max player
+		int resultVal= isMax?-Integer.MAX_VALUE:Integer.MAX_VALUE; //AI = min start result, player = max start result
+		for(List<Token> aMove : possibleMoves(game.getBoard())){
+			//System.out.println("Testing move: "+aMove);
+			Game newGame = doMove(game, aMove, ai);
+			int nextVal=minmax(newGame, !ai, searchDepth-1, color, size, game);
 			
-		}else{ //gut, wenn gegner farbe sammelt und selbst noch nicht
-			if(opponentList.containsKey(gainedColor)){
-				result = opponentList.get(gainedColor); //TODO
-				if((opponentList.get(gainedColor)+move.size()) == game.getSize()) result = Integer.MAX_VALUE;
-			}else{
-				result = move.size();
+			//max with higher value or min with lower value = set this value
+			if(isMax && (nextVal>=resultVal)){
+				//System.out.println("MAX");
+				resultVal=nextVal;
+			}
+			if(!isMax && (nextVal<=resultVal)){
+				//System.out.println("MIN");
+				resultVal=nextVal;
 			}
 		}
+		System.out.println("MinMaxResult:"+resultVal);
+		return resultVal;
+	}
+	
+	public static int evalMove(Game game, boolean ai, Color c, int size, int impact){
+		int result = 0;
+		Color gainedColor = c;
+		Map<Color,Integer> playerList, aiList;
+
+		playerList = game.getPlayerStones();
+		aiList = game.getAiStones();
+
+		if(!playerList.containsKey(gainedColor) && !aiList.containsKey(gainedColor) && size == game.getSize()){
+			result += 1000;
+		} if(playerList.containsKey(gainedColor) && ((size + playerList.get(gainedColor))==game.getSize())){
+			result += 100;
+		} else if(aiList.containsValue(gainedColor) && ((size + aiList.get(gainedColor))==game.getSize())){
+			result += 100;
+		} else if(playerList.containsKey(gainedColor) && !aiList.containsKey(gainedColor)){
+			result -= 1;
+		} else if(!playerList.containsKey(gainedColor) && aiList.containsKey(gainedColor)){
+			result += 1;
+		} else if(playerList.containsKey(gainedColor) && aiList.containsKey(gainedColor)){
+			result = 0;
+		} else if(!playerList.containsKey(gainedColor) && !aiList.containsKey(gainedColor)) {
+			result += 1;
+		}
+		if(!ai){
+			result = result *(-1);
+		}
+		//System.out.println("Result("+getColorNamen(gainedColor)+"): ["+result+"]");
 		return result;
 	}
 	
+	//returns all possible Moves as list of Tokens, ordered by color
 	public static List<List<Token>> possibleMoves(Board board){
 		List<Token> takeableStones = Game.takeableStones(board);
 		List<List<Token>> result = new LinkedList<List<Token>>();
@@ -108,20 +118,58 @@ public class AI {
 			}
 		}
 		for(Color c : takeableColors.keySet()){
-			List<Token> colorSet = takeableColors.get(c);
-			int numColorStones = colorSet.size();
-			Set<List<Token>> colorMoves = new HashSet<List<Token>>();
-			for(int i = 0; i < numColorStones; i++){
-				List<Token> move = new LinkedList<Token>();
-				for(int j=0; j<numColorStones-1; j++){
-					if((i & (1 << j)) != 0) {
-						move.add(colorSet.get(j));
-					}
-				}
-				colorMoves.add(move);
-			}
-			result.add(colorSet);
+			List<List<Token>> tokens = powerset(takeableColors.get(c));
+			tokens.remove(new ArrayList<Token>()); //remove empty sets
+			result.addAll(tokens);
 		}
+		//System.out.println("Possible Moves: "+result);
 		return result;
 	}
+	
+	//returns powerset of takeable stones of one color
+	private static <T> List<List<T>> powerset(Collection<T> list) {
+		  List<List<T>> ps = new ArrayList<List<T>>();
+		  ps.add(new ArrayList<T>());   // add the empty set
+		 
+		  // for every item in the original list
+		  for (T item : list) {
+		    List<List<T>> newPs = new ArrayList<List<T>>();
+		 
+		    for (List<T> subset : ps) {
+		      // copy all of the current powerset's subsets
+		      newPs.add(subset);
+		 
+		      // plus the subsets appended with the current item
+		      List<T> newSubset = new ArrayList<T>(subset);
+		      newSubset.add(item);
+		      if(!newSubset.isEmpty()) newPs.add(newSubset);
+		    }
+		 
+		    // powerset is now powerset of list.subList(0, list.indexOf(item)+1)
+		    ps = newPs;
+		  }
+		  return ps;
+		}
+	
+	public static String getColorNamen(Color colorParam) {
+        try {
+            //first read all fields in array
+            Field[] field = Class.forName("java.awt.Color").getDeclaredFields();
+            for (Field f : field) {
+                String colorName = f.getName();
+                Class<?> t = f.getType();
+                // System.out.println(f.getType());
+                // check only for constants - "public static final Color"
+                if (t == java.awt.Color.class) {
+                    Color defined = (Color) f.get(null);
+                    if (defined.equals(colorParam)) {
+                        return colorName.toUpperCase();
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error... " + e.toString());
+        }
+        return "NO_MATCH";
+    }
 }
